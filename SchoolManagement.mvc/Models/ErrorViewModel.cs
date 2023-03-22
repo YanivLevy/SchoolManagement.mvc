add08f491e669acef90fa9472935c77c1d56eee0
yaniv <yaniv@YANIV_HP_CIO>
2023-03-22 13:24:10 +0200

    Add project files.

---
+++ SchoolManagement.mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SchoolManagement.mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
